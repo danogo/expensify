c616a196a9259b5e00b388b02fea9ec63c86eb5f
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-11-05 10:23:20 +0100

    added more action generators for reducers

--- src/redux-expensify.js
+++ src/redux-expensify.js
@@ -14,6 +14,7 @@ const addExpense = ({description= '', note = '', amount = 0, createdAt = 0} = {}
     }
   }
 );
+
 // REMOVE_EXPENSE
 const removeExpense = ({ id } = {}) => (
   {
@@ -23,8 +24,23 @@ const removeExpense = ({ id } = {}) => (
     }
   }
 );
+
 // EDIT_EXPENSE
+const editExpense = (id, updates) => (
+  {
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+  }
+);
+
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => (
+  {
+    type: 'SET_TEXT_FILTER',
+    text
+  }
+);
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -39,6 +55,8 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
       return [...state, action.expense];
     case 'REMOVE_EXPENSE':
       return state.filter(expense => expense.id !== action.expense.id);
+    case 'EDIT_EXPENSE':
+      return state.map(expense => ({...expense, ...action.updates}));
     default:
       return state;
   }
@@ -53,8 +71,10 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch(action.type) {
+    case 'SET_TEXT_FILTER':
+      return {...state, text: action.text};
     default:
-    return state;
+      return state;
   }
 }
 
@@ -64,14 +84,18 @@ const rootReducer = combineReducers({
 });
 const store = createStore(rootReducer);
 
+// run this function everytime actions is dispatched and state changes
 store.subscribe(() => {
   console.log(store.getState());
-})
+});
 // Dispatch returns action object we have dispatched, so we can save it and use it later
 const expenseOne = store.dispatch(addExpense({description: 'my bills', amount: 400}));
-const expenseTwo = store.dispatch(addExpense({description: 'witcher game', amount: 5400}));
+const expenseTwo = store.dispatch(addExpense({description: 'witcher game', amount: 9000}));
 console.log('expenseOne', expenseOne);
 store.dispatch(removeExpense({id: expenseOne.expense.id}));
+store.dispatch(editExpense(expenseTwo.expense.id, {amount: 1337}));
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 const demoState = {
   expenses: [
@@ -90,3 +114,15 @@ const demoState = {
     endDate: undefined
   }
 };
+
+const user = {
+  name: 'Ciri',
+  age: 50
+};
+
+const user2 = Object.assign({}, user, {name: 'Bob'});
+const user3 = { ...user, age: 15};
+console.log('============= Spread Operator =================');
+console.log('user: ', user);
+console.log('user2: ', user2);
+console.log('user3: ', user3);
