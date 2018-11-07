093748189991acdc426552b90b526f3b1de754ef
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-11-07 16:11:11 +0100

    added ExpenseListFilters - input to set text filter, added button for removingindividual expense

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = (props) => (
+const ExpenseListItem = ({description, amount, createdAt, dispatch, id}) => (
   <div>
-    <h3>{props.description}</h3>
-    <p>Amount: {props.amount}</p>
-    <p>Created at: {props.createdAt}</p>
+    <h3>{description}</h3>
+    <p>Amount: {amount}</p>
+    <p>Created at: {createdAt}</p>
+    <button onClick={(e) => {
+      dispatch(removeExpense({id}));
+    }}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+
+// here we connect just to get dispatch method to our WrappedComponent props
+export default connect()(ExpenseListItem);
