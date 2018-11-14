421ae6ff385f30c7eb16c29528074366c18146f4
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-11-14 17:25:41 +0100

    created mapDispatchToProps for AddExpensePage

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -8,11 +8,20 @@ const AddExpensePage = props => (
     <h1>Add expense</h1>
     <ExpenseForm 
       onSubmit={expense => {
-        props.dispatch(addExpense(expense));
+        // props.dispatch(addExpense(expense));
+        props.onSubmit(expense);
         props.history.push('/');
       }}
     />
   </div>
 );
 
-export default connect()(AddExpensePage);
+const mapDispatchToProps = dispatch => {
+  return {
+    onSubmit: expense => {
+      dispatch(addExpense(expense));
+    }
+  }
+};
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
